6a8d7e0434b36a8283f787e738e1d9c96bd82523
luiznaiper <luiz_naiper@hotmail.com>
2023-05-31 07:36:08 -0600

    :construction: Add main pages

---
+++ src/Pages/App/index.tsx
@@ -0,0 +1,23 @@
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SignIn from '../SignIn'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-100">Qué pedo</div>
+      <Home />
+      <MyAccount />
+      <MyOrder />
+      <MyOrders />
+      <NotFound />
+      <SignIn />
+    </>
+  )
+}
+
+export default App
