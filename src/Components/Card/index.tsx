b9c9f4f1d93846618bbd74bc0987681267517cfa
luiznaiper <luiz_naiper@hotmail.com>
2023-06-01 09:46:48 -0500

    :construction: Add items with useState, useEffect and grids

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,22 +1,34 @@
-const Card = () => {
+interface CardProps {
+  data: {
+    title: string
+    price: number
+    images: string[]
+    category: {
+      name: string
+    }
+  }
+}
+
+const Card: React.FC<CardProps> = ({ data }) => {
+  const { title, price, images, category } = data
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-          Electronics
+          {category.name}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="Headphones"
+          src={images[0]}
+          alt={title}
         />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 pb-1.5">
           +
         </div>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lg font-medium">$4,000</span>
+        <span className="text-sm font-light">{title}</span>
+        <span className="text-lg font-medium">${price}</span>
       </p>
     </div>
   )
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react'
 import Card from '../../Components/Card'
 import Layout from '../../Components/Layout'
 
 const Home = () => {
+  const [items, setItems] = useState([])
+
+  const url = 'https://api.escuelajs.co/api/v1/products'
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetch(url)
+      const data = await resp.json()
+      setItems(data)
+    }
+    fetchData()
+  }, [])
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => {
+          const { id } = item
+          return <Card key={id} data={item} />
+        })}
+      </div>
     </Layout>
   )
 }
